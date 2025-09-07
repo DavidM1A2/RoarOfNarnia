f8f30c34004a450d41ddd72f111aa0eb7a48886c
David Slovikosky <davidslovikosky@gmail.com>
2025-09-07 16:45:47 -0700

    Fix door, trapdoor, and sapling renderer

--- src/main/java/com/dslovikosky/narnia/common/event/datagenproviders/ModModelProvider.java
+++ src/main/java/com/dslovikosky/narnia/common/event/datagenproviders/ModModelProvider.java
@@ -6,22 +6,25 @@ import com.dslovikosky.narnia.common.constants.ModItems;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.MultiVariant;
+import net.minecraft.client.data.models.model.ModelTemplate;
 import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.client.data.models.model.TextureMapping;
 import net.minecraft.client.data.models.model.TexturedModel;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.data.BlockFamily;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
-
-import java.util.HashMap;
-import java.util.Map;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.client.model.generators.template.ExtendedModelTemplate;
 
 public class ModModelProvider extends ModelProvider {
-    //    private final Set<Block> blocksWithCustomItemRenderer = Set.of(ModBlocks.RING_BOX.get());
-    private final Map<String, ResourceLocation> materialToTexture = new HashMap<>();
-
     public ModModelProvider(final PackOutput output) {
         super(output, Constants.MOD_ID);
-//        materialToTexture.put("world_wood", modLocation(BLOCK_FOLDER + "/world_wood_planks"));
+    }
+
+    private static ExtendedModelTemplate withRenderType(final ModelTemplate modelTemplate, final String renderType) {
+        return modelTemplate.extend().renderType(renderType).build();
     }
 
     @Override
@@ -30,19 +33,20 @@ public class ModModelProvider extends ModelProvider {
 
         blockModels.woodProvider(ModBlocks.WORLD_WOOD_LOG.get()).logWithHorizontal(ModBlocks.WORLD_WOOD_LOG.get()).wood(ModBlocks.WORLD_WOOD.get());
         blockModels.woodProvider(ModBlocks.STRIPPED_WORLD_WOOD_LOG.get()).logWithHorizontal(ModBlocks.STRIPPED_WORLD_WOOD_LOG.get()).wood(ModBlocks.STRIPPED_WORLD_WOOD.get());
-        blockModels.createTintedLeaves(ModBlocks.WORLD_WOOD_LEAVES.get(), TexturedModel.LEAVES, -12012264);
-        blockModels.createCrossBlockWithDefaultItem(ModBlocks.WORLD_WOOD_SAPLING.get(), BlockModelGenerators.PlantType.TINTED);
-        blockModels.family(ModBlocks.WORLD_WOOD_PLANKS.get()).generateFor(new BlockFamily.Builder(ModBlocks.WORLD_WOOD_PLANKS.get())
-                .button(ModBlocks.WORLD_WOOD_BUTTON.get())
-                .door(ModBlocks.WORLD_WOOD_DOOR.get())
-                .fence(ModBlocks.WORLD_WOOD_FENCE.get())
-                .fenceGate(ModBlocks.WORLD_WOOD_FENCE_GATE.get())
-                .slab(ModBlocks.WORLD_WOOD_SLAB.get())
-                .stairs(ModBlocks.WORLD_WOOD_STAIR.get())
-                .pressurePlate(ModBlocks.WORLD_WOOD_PRESSURE_PLATE.get())
-                .sign(ModBlocks.WORLD_WOOD_WALL_SIGN.get(), ModBlocks.WORLD_WOOD_STANDING_SIGN.get())
-                .trapdoor(ModBlocks.WORLD_WOOD_TRAP_DOOR.get())
-                .getFamily());
+        blockModels.createTintedLeaves(ModBlocks.WORLD_WOOD_LEAVES.get(), TexturedModel.LEAVES, 0xFF28DC00);
+        createSapling(blockModels, ModBlocks.WORLD_WOOD_SAPLING.get(), BlockModelGenerators.PlantType.TINTED);
+        blockModels.family(ModBlocks.WORLD_WOOD_PLANKS.get())
+                .generateFor(new BlockFamily.Builder(ModBlocks.WORLD_WOOD_PLANKS.get())
+                        .button(ModBlocks.WORLD_WOOD_BUTTON.get())
+                        .fence(ModBlocks.WORLD_WOOD_FENCE.get())
+                        .fenceGate(ModBlocks.WORLD_WOOD_FENCE_GATE.get())
+                        .slab(ModBlocks.WORLD_WOOD_SLAB.get())
+                        .stairs(ModBlocks.WORLD_WOOD_STAIR.get())
+                        .pressurePlate(ModBlocks.WORLD_WOOD_PRESSURE_PLATE.get())
+                        .sign(ModBlocks.WORLD_WOOD_WALL_SIGN.get(), ModBlocks.WORLD_WOOD_STANDING_SIGN.get())
+                        .getFamily());
+        createOrientableTrapdoor(blockModels, ModBlocks.WORLD_WOOD_TRAP_DOOR.get());
+        createDoor(blockModels, ModBlocks.WORLD_WOOD_DOOR.get());
         blockModels.createHangingSign(ModBlocks.WORLD_WOOD_PLANKS.get(), ModBlocks.WORLD_WOOD_CEILING_HANGING_SIGN.get(), ModBlocks.WORLD_WOOD_WALL_HANGING_SIGN.get());
 
         itemModels.generateFlatItem(ModItems.YELLOW_RING.get(), ModelTemplates.FLAT_ITEM);
@@ -51,5 +55,39 @@ public class ModModelProvider extends ModelProvider {
 
         itemModels.generateFlatItem(ModItems.WORLD_WOOD_BOAT.get(), ModelTemplates.FLAT_ITEM);
         itemModels.generateFlatItem(ModItems.WORLD_WOOD_CHEST_BOAT.get(), ModelTemplates.FLAT_ITEM);
+    }
+
+    private void createSapling(final BlockModelGenerators blockModels, final Block block, final BlockModelGenerators.PlantType plantType) {
+        blockModels.registerSimpleItemModel(block, plantType.createItemModel(blockModels, block));
+        final TextureMapping textureMapping = plantType.getTextureMapping(block);
+        final MultiVariant multiVariant = BlockModelGenerators.plainVariant(plantType.getCross()
+                .extend()
+                .renderType(RenderType.cutout().getName())
+                .build()
+                .create(block, textureMapping, blockModels.modelOutput));
+        blockModels.blockStateOutput.accept(BlockModelGenerators.createSimpleBlock(block, multiVariant));
+    }
+
+    private void createOrientableTrapdoor(final BlockModelGenerators blockModels, final Block trapdoorBlock) {
+        final TextureMapping textureMapping = TextureMapping.defaultTexture(trapdoorBlock);
+        final MultiVariant multiVariantTop = BlockModelGenerators.plainVariant(withRenderType(ModelTemplates.ORIENTABLE_TRAPDOOR_TOP, RenderType.cutout().getName()).create(trapdoorBlock, textureMapping, blockModels.modelOutput));
+        final ResourceLocation resourceLocation = withRenderType(ModelTemplates.ORIENTABLE_TRAPDOOR_BOTTOM, RenderType.cutout().getName()).create(trapdoorBlock, textureMapping, blockModels.modelOutput);
+        final MultiVariant multiVariantOpen = BlockModelGenerators.plainVariant(withRenderType(ModelTemplates.ORIENTABLE_TRAPDOOR_OPEN, RenderType.cutout().getName()).create(trapdoorBlock, textureMapping, blockModels.modelOutput));
+        blockModels.blockStateOutput.accept(BlockModelGenerators.createOrientableTrapdoor(trapdoorBlock, multiVariantTop, BlockModelGenerators.plainVariant(resourceLocation), multiVariantOpen));
+        blockModels.registerSimpleItemModel(trapdoorBlock, resourceLocation);
+    }
+
+    private void createDoor(final BlockModelGenerators blockModels, final Block doorBlock) {
+        final TextureMapping textureMapping = TextureMapping.door(doorBlock);
+        final MultiVariant multiVariantBottomLeft = BlockModelGenerators.plainVariant(withRenderType(ModelTemplates.DOOR_BOTTOM_LEFT, RenderType.cutout().getName()).create(doorBlock, textureMapping, blockModels.modelOutput));
+        final MultiVariant multiVariantBottomLeftOpen = BlockModelGenerators.plainVariant(withRenderType(ModelTemplates.DOOR_BOTTOM_LEFT_OPEN, RenderType.cutout().getName()).create(doorBlock, textureMapping, blockModels.modelOutput));
+        final MultiVariant multiVariantBottomRight = BlockModelGenerators.plainVariant(withRenderType(ModelTemplates.DOOR_BOTTOM_RIGHT, RenderType.cutout().getName()).create(doorBlock, textureMapping, blockModels.modelOutput));
+        final MultiVariant multiVariantBottomRightOpen = BlockModelGenerators.plainVariant(withRenderType(ModelTemplates.DOOR_BOTTOM_RIGHT_OPEN, RenderType.cutout().getName()).create(doorBlock, textureMapping, blockModels.modelOutput));
+        final MultiVariant multiVariantTopLeft = BlockModelGenerators.plainVariant(withRenderType(ModelTemplates.DOOR_TOP_LEFT, RenderType.cutout().getName()).create(doorBlock, textureMapping, blockModels.modelOutput));
+        final MultiVariant multiVariantTopLeftOpen = BlockModelGenerators.plainVariant(withRenderType(ModelTemplates.DOOR_TOP_LEFT_OPEN, RenderType.cutout().getName()).create(doorBlock, textureMapping, blockModels.modelOutput));
+        final MultiVariant multiVariantTopRight = BlockModelGenerators.plainVariant(withRenderType(ModelTemplates.DOOR_TOP_RIGHT, RenderType.cutout().getName()).create(doorBlock, textureMapping, blockModels.modelOutput));
+        final MultiVariant multiVariantTopRightOpen = BlockModelGenerators.plainVariant(withRenderType(ModelTemplates.DOOR_TOP_RIGHT_OPEN, RenderType.cutout().getName()).create(doorBlock, textureMapping, blockModels.modelOutput));
+        blockModels.registerSimpleFlatItemModel(doorBlock.asItem());
+        blockModels.blockStateOutput.accept(BlockModelGenerators.createDoor(doorBlock, multiVariantBottomLeft, multiVariantBottomLeftOpen, multiVariantBottomRight, multiVariantBottomRightOpen, multiVariantTopLeft, multiVariantTopLeftOpen, multiVariantTopRight, multiVariantTopRightOpen));
     }
 }
