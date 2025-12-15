9eb63b5d1ac914ec0d9959682aa1f1fa4b9adf38
David Slovikosky <davidslovikosky@gmail.com>
2025-12-15 00:15:41 -0800

    Update schematics with runes

--- src/main/java/com/dslovikosky/narnia/common/model/attachment_type/TicksInWoodBetweenTheWorldsSyncHandler.java
+++ src/main/java/com/dslovikosky/narnia/common/model/attachment_type/TicksInWoodBetweenTheWorldsSyncHandler.java
@@ -26,6 +26,14 @@ public class TicksInWoodBetweenTheWorldsSyncHandler implements AttachmentSyncHan
 
     @Override
     public @Nullable Integer read(IAttachmentHolder holder, RegistryFriendlyByteBuf buf, @Nullable Integer previousValue) {
-        return Objects.requireNonNullElseGet(previousValue, () -> ByteBufCodecs.INT.decode(buf));
+        return Objects.requireNonNullElseGet(previousValue, () -> {
+            // Try-catch seems to be needed due to a bug??? Initial sync always throws
+            // "java.lang.IndexOutOfBoundsException: readerIndex(1) + length(4) exceeds writerIndex(1): UnpooledHeapByteBuf(ridx: 1, widx: 1, cap: 1/1)"
+            try {
+                return ByteBufCodecs.INT.decode(buf);
+            } catch (final IndexOutOfBoundsException e) {
+                return 0;
+            }
+        });
     }
 }
